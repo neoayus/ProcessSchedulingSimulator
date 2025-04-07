656aa62cc61e658d6884d29cb9babd1dba21f148
neoayus <ayus43@outlook.com>
2025-04-07 22:55:41 +0530

    fix: fix typos

--- src/Main.java
+++ src/Main.java
@@ -8,63 +8,63 @@ public class Main{
         
         Scanner scan = new Scanner(System.in);         
 
-        System.out.println("Enter the context switch : ");  
+        System.out.print("Enter the context switch : ");  
         int context = scan.nextInt(); 
         
-        System.out.println("Enter the # of processes : ");
+        System.out.print("Enter the # of processes : ");
         // reading the total number of processes  
         int numberOfTotalProcesses = scan.nextInt();  
         
          
-        System.out.println("Enter the degree of MP : ");
+        System.out.print("Enter the degree of MP : ");
         int degreeOfMP = scan.nextInt(); 
         
-        System.out.println("Enter the number of bursts in each process : ");
+        System.out.print("Enter the number of bursts in each process : ");
         int numberOfBursts = scan.nextInt(); 
 
 
         // CPU Burst range 
-        System.our.println("Enter the range of CPU bursts : ");
-        System.out.prinln("Min: ");
+        System.out.print("Enter the range of CPU bursts : ");
+        System.out.print("Min: ");
         int minCPU = scan.nextInt();
-        System.out.prinln("Max: ");
+        System.out.print("Max: ");
         int maxCPU = scan.nextInt();
         
 
         // I/O burst range 
-        System.out.println("Enter the range of IO bursts : ");
-        System.out.println("Min: ");
+        System.out.print("Enter the range of IO bursts : ");
+        System.out.print("Min: ");
         int minIO = scan.nextInt();
-        System.out.println("Max: ");
+        System.out.print("Max: ");
         int maxIO = scan.nextInt();
 
         
         // Priority range 
-        System.out.println("Enter the range of priority : ");
-        System.out.println("Min :");
+        System.out.print("Enter the range of priority : ");
+        System.out.print("Min :");
         int minPriority = scan.nextInt() ; 
-        System.out.println("Max :");
+        System.out.print("Max :");
         int maxPriority = scan.nextInt(); 
         
 
         // Arrival time range 
-        System.out.println("Enter the range of Initial Arrival Times : ");
-        System.out.println("Min: ");
+        System.out.print("Enter the range of Initial Arrival Times : ");
+        System.out.print("Min: ");
         int minArrival = scan.nextInt();
-        System.out.println("Max: ");
+        System.out.print("Max: ");
         int maxArrival = scan.nextInt();
 
         // I/ for tau and alpha for prediction (SJF/Exponentials)
-        System.out.println("Enter initial Tau :");
+        System.out.print("Enter initial Tau :");
         int initialTau = scan.nextInt(); 
         
-        System.out.println("Enter alpha :");
+        System.out.print("Enter alpha :");
         double alpha = scan.nextDouble();
         
         
         // Random Type 
-        System.out.println("Enter your type of generating random number of CPU/IO bursts : ");
-        System.out.println("(0) for Gaussian / (other) for binomial :");
+        System.out.print("Enter your type of generating random number of CPU/IO bursts : ");
+        System.out.print("(0) for Gaussian / (other) for binomial : ");
         int type = scan.nextInt();
         boolean randomType = type == 0 ? true : false ;  
         
